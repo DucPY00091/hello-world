8521a1d6305c7535afad1d2e78d10fd351cf97ab
Duc <ducntpy00091@gmail.com>
2024-11-02 18:01:00 +0700

    demo

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,25 @@
+
 import { createRouter, createWebHistory } from 'vue-router';
-import About from '../components/About.vue';
-import HomePage from '../components/Home.vue'
-import BlogPage from '../components/Blog.vue'
+ import Bai1 from '../components/Lab1/Bai1.vue';
+ import Bai2 from '../components/Lab1/Bai2.vue';
+ import Bai3 from '../components/Lab1/Bai3.vue';
+
+
+
+// import About from '../components/About.vue';
+// import Lab1 from '../components/lab1/b3.vue'
+// import BlogPage from '../components/Blog.vue'
 
 const routes = [
-    { path: '/', component: HomePage },
-    { path: '/blog', component: BlogPage },
-    { path: '/about', component: About }
+
+     { path: '/Lab1/B1', component: Bai1 },
+     { path: '/Lab1/B2', component: Bai2 },
+     { path: '/Lab1/B3', component: Bai3 },
+
+
+    // { path: '/Lab1/b1', component: Lab1 },
+    // { path: '/blog', component: BlogPage },
+    // { path: '/about', component: About }
 ];
 
 const router = createRouter({
